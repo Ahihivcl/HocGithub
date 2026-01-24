175f0ec550f69770794144aea7bb2d3c64a8e653
TVipProMax <vantienlai134@gmail.com>
2026-01-24 22:01:18 +0700

    Heheheheheheh

---
+++ cmake-sfml-project/src/main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        // Clear the window with black color
+        window.clear();
+        window.display();
+    }
+}
